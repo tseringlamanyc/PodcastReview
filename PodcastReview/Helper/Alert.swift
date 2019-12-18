b5e040f2243584b1a64dd8b9a601fda492ca6d64
Tsering Lama <tseringlama@pursuit.org>
2019-12-18 10:16:26 -0500

    added a test for the podcast and passed successfully

---
+++ PodcastReview/Helper/Alert.swift
@@ -0,0 +1,18 @@
+//
+//  Alert.swift
+//  PodcastReview
+//
+//  Created by Tsering Lama on 12/17/19.
+//  Copyright © 2019 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+  func showAlert(title: String, message: String, completion: ((UIAlertAction) -> Void)? = nil) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "Ok", style: .default, handler: completion)
+    alertController.addAction(okAction)
+    present(alertController, animated: true, completion: nil)
+  }
+}
